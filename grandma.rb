0368887d7d966b325525cc78569f15598d1bd1bd
Peter Nguyen <peternguyen160@gmail.com>
2019-02-22 09:51:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -24,7 +24,7 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
 
     elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
+      return "HUH?! SPEAK UP, SONNY!"
 
     else
   		return "NO, NOT SINCE 1938!"
